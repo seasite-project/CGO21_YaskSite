17477c1cd66a5f83148ce0517aad611c3abe620a
Johannes Seiferth <johannes.seiferth@uni-bayreuth.de>
2020-12-09 18:41:39 +0100

    Install script Singularity

---
+++ install_singularity.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y build-essential libseccomp-dev pkg-config squashfs-tools cryptsetup 
+
+export VERSION=1.15.6 OS=linux ARCH=amd64 && \  # Replace the values as needed
+wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \ # Downloads the required Go package
+sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \ # Extracts the archive
+rm go$VERSION.$OS-$ARCH.tar.gz    # Deletes the ``tar`` file
+
+
+echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc && source ~/.bashrc
+
+rm -r singularity/
+export VERSION=3.7.0 && wget https://github.com/hpcng/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && tar -xzf singularity-${VERSION}.tar.gz && cd singularity
+
+./mconfig && make -C builddir && sudo make -C builddir install
