d9760e097ab0ec3cd5520c193f69fd47a94fee9b
joseiferth <48958482+joseiferth@users.noreply.github.com>
2020-12-10 10:17:58 +0100

    Update install_singularity.sh

--- install_singularity.sh
+++ install_singularity.sh
@@ -2,10 +2,7 @@
 
 sudo apt-get update && sudo apt-get install -y build-essential libseccomp-dev pkg-config squashfs-tools cryptsetup 
 
-export VERSION=1.15.6 OS=linux ARCH=amd64 && \  # Replace the values as needed
-wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \ # Downloads the required Go package
-sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \ # Extracts the archive
-rm go$VERSION.$OS-$ARCH.tar.gz    # Deletes the ``tar`` file
+export VERSION=1.15.6 OS=linux ARCH=amd64 && wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && rm go$VERSION.$OS-$ARCH.tar.gz
 
 
 echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc && source ~/.bashrc
