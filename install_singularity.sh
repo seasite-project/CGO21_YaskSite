ee92ceca2923e1e363c6308bfa532cecf0f975dc
joseiferth <48958482+joseiferth@users.noreply.github.com>
2020-12-10 10:26:50 +0100

    Update install_singularity.sh

--- install_singularity.sh
+++ install_singularity.sh
@@ -2,11 +2,11 @@
 
 sudo apt-get update && sudo apt-get install -y build-essential libseccomp-dev pkg-config squashfs-tools cryptsetup 
 
-export VERSION=1.15.6 OS=linux ARCH=amd64 && wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && rm go$VERSION.$OS-$ARCH.tar.gz
+export VERSION=1.15.6 OS=linux ARCH=amd64 && rm go$VERSION.$OS-$ARCH.tar.gz && wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && rm go$VERSION.$OS-$ARCH.tar.gz
 
 echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc && source ~/.bashrc
 
-rm -r singularity/
+rm -rf singularity/
 export VERSION=3.7.0 && wget https://github.com/hpcng/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && tar -xzf singularity-${VERSION}.tar.gz && cd singularity
 
 ./mconfig && make -C builddir && sudo make -C builddir install
